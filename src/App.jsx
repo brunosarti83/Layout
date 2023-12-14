3d2ba3e39ccea5f935229c622fa464b460acc83b
brunosarti83 <brunosarti.bs@gmail.com>
2023-12-14 18:12:01 -0300

    resizable one div

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Layout from "./Components/Layout/Layout"
+
+
+function App() {
+
+  return (
+    <div className="w-full h-screen bg-red-500 flex justify-center items-center">
+      <Layout />
+    </div>
+  )
+}
+
+export default App
