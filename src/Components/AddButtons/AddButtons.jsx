e6295e7bef3d1b1896428eb9fbe813d9927674ad
brunosarti83 <brunosarti.bs@gmail.com>
2024-01-11 09:14:31 -0300

    WIP(AddButton Styles)

--- src/Components/AddButtons/AddButtons.jsx
+++ src/Components/AddButtons/AddButtons.jsx
@@ -1,29 +1,35 @@
 /* eslint-disable react/prop-types */
 import { addToLayout } from "../../redux/actions";
 import { useDispatch } from "react-redux";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function AddButtons() {
   const dispatch = useDispatch();
 
   return (
-    <div className="flex gap-1">
+    <div className="flex gap-[2px] h-[45px] text-white">
       <button
         onClick={() => dispatch(addToLayout("column", "right"))}
-        className="bg-gray-300 h-6 px-2 text-center align-middle"
+        className="bg-sky-500 h-full w-[20px] rounded-l-[10px] text-center align-middle"
       >
-        {"<"}
+        <IoIosArrowBack className="mx-auto text-lg" />
       </button>
-      <button
-        onClick={() => dispatch(addToLayout("row"))}
-        className="bg-gray-300 h-6 px-2 text-center align-middle"
-      >
-        {"v"}
-      </button>
+      <div className="flex flex-col gap-[2px]">
+        <div className="bg-sky-500 h-1/2"></div>
+        <button
+          onClick={() => dispatch(addToLayout("row"))}
+          className="bg-sky-500 h-1/2 w-[50px] text-center align-middle"
+        >
+          <IoIosArrowDown className="mx-auto text-lg" />
+        </button>
+      </div>
       <button
         onClick={() => dispatch(addToLayout("column", "left"))}
-        className="bg-gray-300 h-6 px-2 text-center align-middle"
+        className="bg-sky-500 h-full w-[20px] rounded-r-[10px] text-center align-middle"
       >
-        {">"}
+        <IoIosArrowForward className="mx-auto text-lg" />
       </button>
     </div>
   );
