498fbbb058848dd0d48ce405e71cd1a74b95e30b
brunosarti83 <brunosarti.bs@gmail.com>
2024-01-13 11:32:11 -0300

    ThreeDots close menu onMouseLeave

--- src/Components/ThreeDotsMenu/ThreeDotsMenu.jsx
+++ src/Components/ThreeDotsMenu/ThreeDotsMenu.jsx
@@ -4,18 +4,18 @@ import { BsThreeDots } from "react-icons/bs";
 function ThreeDotsMenu({ onClose }) {
   const [open, setOpen] = useState(false);
   return (
-    <div className="relative">
+    <div className="relative" onMouseLeave={() => setOpen(false)}>
       <button onClick={() => setOpen(!open)} className="bg-transparent flex">
         <BsThreeDots className="text-lg m-auto text-gray-600" />
       </button>
       <div
-        className={`absolute top-[100%] left-[50%] transition-all duration-150 ${
+        className={`absolute top-[100%] left-[50%] transition-all duration-150 p-[4px] ${
           open ? "opacity-1" : "opacity-0 hidden translate-y-[-20px]"
         }`}
       >
         <button
           onClick={onClose}
-          className="w-[200px] py-2 bg-gray-400/80 hover:bg-gray-400"
+          className="w-[200px] py-2 bg-gray-400/80 hover:bg-gray-400 rounded-[4px]"
         >
           Close
         </button>
